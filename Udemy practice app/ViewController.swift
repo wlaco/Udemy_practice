12738b8616871161a807d70ddf6e7d159b6598d8
Will Laco <wlaco@Wills-MBP.fios-router.home>
2017-04-23 17:53:35 -0400

    Initial Commit

---
+++ Udemy practice app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udemy practice app
+//
+//  Created by Will Laco on 4/23/17.
+//  Copyright (c) 2017 Will Laco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Udemy practice appTests/Udemy_practice_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  Udemy_practice_appTests.swift
+//  Udemy practice appTests
+//
+//  Created by Will Laco on 4/23/17.
+//  Copyright (c) 2017 Will Laco. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Udemy_practice_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
