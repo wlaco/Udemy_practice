fd15c58a8a40fb421e365b11385d2d351f632905
Will Laco <wlaco27@gmail.com>
2017-04-25 20:13:43 -0400

    First commit

--- Udemy practice app/ViewController.swift
+++ Udemy practice app/ViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var pressCount = 0
+    
+    @IBOutlet weak var theLabel: UILabel!
+    
+    @IBAction func theButton(_ sender: AnyObject) {
+        
+        theLabel.text = String(Double(text1.text!)! + Double(text2.text!)!)
+    
+    }
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        theLabel.text = "Welcome to GuyWeather!"
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
